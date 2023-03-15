1dd71f2f87fd7186053d5da3bb76bb26c4ca8a45
Jakub Mikołajek <wiecny00@gmail.com>
2023-03-15 18:30:09 +0100

    Routing

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
 
-echo "Hello";
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url($path, PHP_URL_PATH);
+
+Routing::get('add', 'DefaultController');
+Routing::get('hau', 'DefaultController');
+Routing::get('login', 'DefaultController');
+Routing::get('register', 'DefaultController');
+Routing::get('search', 'DefaultController');
+Routing::get('workofart', 'DefaultController');
+
+Routing::run($path);
---
+++ src/controllers/DefaultController.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once 'AppController.php';
+
+class DefaultController extends AppController {
+
+    public function add()
+    {
+        $this->render('add');
+    }
+
+    public function hau()
+    {
+        $this->render('hau');
+    }
+
+    public function login()
+    {
+        $this->render('login');
+    }
+
+    public function register()
+    {
+        $this->render('register');
+    }
+
+    public function search()
+    {
+        $this->render('search');
+    }
+
+    public function workofart()
+    {
+        $this->render('workofart');
+    }
+
+
+}
